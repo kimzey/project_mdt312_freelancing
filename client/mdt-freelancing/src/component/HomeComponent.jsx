c21c0b713ada4aaba007d29453276db0d8a0919a
“fluke” <araa1365@gmail.com>
2023-11-19 09:43:45 +0700

    update home

--- client/mdt-freelancing/src/component/HomeComponent.jsx
+++ client/mdt-freelancing/src/component/HomeComponent.jsx
@@ -1,26 +1,32 @@
 import "./HomeComponent.css"
 import Navbar from "./NavbarComponent"
+import logo_img from "../assets/logo.png"
+
 
 
 export default function Home() {
   return (
     <div className="Home">
-      <Navbar />
+      <Navbar></Navbar>
 
       <div className="Firstcontainer">
-        <h1 className="text1">
+        <h1 id="text1">
           <br />
-          ค้นหางานที่ใช่สำหรับคุณ
-        </h1>
-        <img src=""></img>
+          ค้นหางานที่ใช่สำหรับคุณ</h1>
+     <br/>  <div id ="logocontainer"> <img src={logo_img} id="logo_webb" alt="logoweb"></img> </div>
       </div>
 
       <div className="Secondcontainer">
-        <p className="text2">
+        <p id="text2">
           งานที่แนะนำ
         </p>
 
-        <p className="rectangle"></p>
+        <div class ="squarecontainer"> 
+        <p class="square">   </p>
+        <p class="square">   </p>
+        <p class="square">   </p>
+        </div>
+
 
       </div>
     </div>
