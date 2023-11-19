5d69d890127ac221b6503c925f44290e3efdffdc
“fluke” <araa1365@gmail.com>
2023-11-20 00:45:57 +0700

    update home

--- client/mdt-freelancing/src/component/HomeComponent.jsx
+++ client/mdt-freelancing/src/component/HomeComponent.jsx
@@ -1,8 +1,6 @@
-import "./HomeComponent.css"
-import Navbar from "./NavbarComponent"
-import logo_img from "../assets/logo.png"
-
-
+import "./HomeComponent.css";
+import Navbar from "./NavbarComponent";
+import logo_img from "../assets/logo.png";
 
 export default function Home() {
   return (
@@ -10,34 +8,33 @@ export default function Home() {
       <div className="Firstcontainer">
         <h1 id="text1">
           <br />
-          ค้นหางานที่ใช่สำหรับคุณ</h1>
-     <br/>  <div id ="logocontainer"> <img src={logo_img} id="logo_webb" alt="logoweb"></img> </div>
-      </div>
-
-      <div className="Secondcontainer">
-        <p id="text2">
-          งานที่แนะนำ
-        </p>
-
-        <div class ="squarecontainer"> 
-        <p class="square">  ... </p>
-        <p class="square">  ...  </p>
-        <p class="square">  ... </p>   
-        <p class="square">  ... </p>        
+          ค้นหางานที่ใช่สำหรับคุณ
+        </h1>
+        <br />
+        <div id="logocontainer">
+         
+          <img src={logo_img} id="logo_webb" alt="logoweb"></img>
         </div>
       </div>
 
-      
-      <div className="thridcontainer">
+      <div className="Secondcontainer">
+        <p id="text2">งานที่แนะนำ</p>
 
-        <h2 id="contact">ติดต่อพวกเรา</h2>
-        <p>Email: info@example.com</p>
-        <p>Phone: +1 (555) 123-4567</p>
-
+        <div class="squarecontainer">
+          <p class="square"> ... </p>
+          <p class="square"> ... </p>
+          <p class="square"> ... </p>
+          <p class="square"> ... </p>
+        </div>
       </div>
 
-
-
+      <footer>
+        <div className="thridcontainer">
+          <h2 id="contact">ติดต่อพวกเรา</h2>
+          <p>Email: info@example.com</p>
+          <p>Phone: +1 (555) 123-4567</p>
+        </div>
+      </footer>
     </div>
   );
 }
